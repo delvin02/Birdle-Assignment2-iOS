b7086d208b007272541218cd5d744c207b01f93e
Delvin <delvin.khor@splose.com>
2025-10-31 10:11:51 +1030

    Initial Commit

---
+++ Birdie-Assignment2-SwiftUI/Birdie_Assignment2_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birdie_Assignment2_SwiftUIApp.swift
+//  Birdie-Assignment2-SwiftUI
+//
+//  Created by Delvin on 31/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Birdie_Assignment2_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birdie-Assignment2-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Birdie-Assignment2-SwiftUI
+//
+//  Created by Delvin on 31/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
